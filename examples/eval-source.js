9f59d5894d5bee9288678d947ca7b78ed6e99a27
Wenson Hsieh <wenson_hsieh@apple.com>
2022-09-07 19:13:14 -0700

    Adjust eval example.

--- examples/eval-source.js
+++ examples/eval-source.js
@@ -2,6 +2,9 @@ function getSource() {
     return `
         (function() {
             console.log("This is a test!");
+            fetch("https://whsieh.github.io/examples/lorem").then(response => {
+                console.log("Request to " + response.url + " returned: " + response.status);
+            });
             return 42;
         })()`;
 }
